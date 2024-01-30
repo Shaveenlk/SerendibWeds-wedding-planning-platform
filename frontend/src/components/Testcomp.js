ed0d1b117c15c49375e25817a1466fc8ea3f5f1d
unknown <shaveendesilva2001@gmail.com>
2024-01-30 14:14:29 +0530

    components and pages folders added

---
+++ frontend/src/components/Testcomp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testcomp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Testcomp
---
+++ frontend/src/pages/Savedweddings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Savedweddings = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Savedweddings
