673a0e7401f2bed59e22109e4bd8563060ab4b4f
Shaveenlk <shaveendesilava2001@gmail.com>
2024-01-31 21:15:28 +0530

    profile component created and create two page folder

---
+++ frontend/src/components/Profilecomp.js
@@ -0,0 +1,57 @@
+import { Box, Grid, Avatar, Typography, Button } from "@mui/material";
+import React from "react";
+
+
+
+const Profilecomp = () => {
+  return (
+    <div className="lol">
+      <Box>
+        <Grid container spacing={2}>
+          {/* Profile Section */}
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={9}
+            lg={9}
+            alignItems="center"
+            justifyContent="center"
+            display="flex"
+      
+          >
+            <Avatar
+              src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              sx={{ width: 100, height: 100,marginTop: 2, ml: 2, mb: 2 }}
+      
+            />
+            <Typography variant="h5" component="div" sx={{ ml: 2, p: 2,fontWeight: 'bold' }}>
+              Hazel & Lucas <br /> lucas@gmail.com
+            </Typography>
+          </Grid>
+          {/* Logout Button Section */}
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            md={3}
+            lg={3}
+            alignItems="center"
+            justifyContent="center"
+            display="flex"
+          >
+            <Button variant="contained"  sx={{borderRadius: 5 ,
+                  marginLeft: { xs: 8, sm: 0 },
+                  marginBottom: { xs: 3, sm: 0 }, // 3px margin on xs (extra small) screens, no margin on sm (small) screens and above
+                   // Adjust this value based on your design preferences
+                }}>
+              Log Out
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+};
+
+export default Profilecomp;
---
+++ frontend/src/pages/todolist/TodoList.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import '../../css/task.css';
+
+const ToDoList = () => {
+  const TaskList = [
+    { id: 1, title: 'Research photographs in my area' },
+    { id: 2, title: 'Wedding Dress Shopping' },
+    { id: 3, title: 'Book Hair and Makeup Artists' },
+    { id: 4, title: 'Arrange djs' },
+    { id: 5, title: 'Confirm Honeymoon Details' },
+    { id: 6, title: 'Research photographs in my area1' },
+    { id: 7, title: 'Wedding Dress Shopping2' },
+    { id: 8, title: 'Book Hair and Makeup Artists3' },
+    { id: 9, title: 'Arrange djs4' },
+    { id: 10, title: 'Confirm Honeymoon Details5' },
+  ];
+
+  
+ 
+  return (
+    <div className='task-list'>
+     
+    </div>
+  );
+};
+
+export default ToDoList;
