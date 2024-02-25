db3a9cfbc274030dd4c00a0e977e80e90b9f3c2e
Nishadi Perera <124398259+nishadi220@users.noreply.github.com>
2024-02-25 11:33:26 +0530

    Made services and vendors APIs functional

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/userRoutes.js";
 
+
 const app =express();
 app.use(bodyParser.json());
 app.use(cors());
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express, {Router} from 'express';
 import { checkuser, createUser, getUserData } from '../controller/userController.js';
 import { deleteTodo, getTodo } from '../controller/todoController.js';
+import {getVendors,getVendor} from "../controller/vendorController.js"
+import { getServices } from '../controller/serviceController.js';
 
 const route =express.Router();
 
@@ -9,5 +11,9 @@ route.get('/checkUser/:firebaseUserId', checkuser);
 route.get('/getuser/:firebaseUserId', getUserData);
 route.get('/todo/:firebaseUserId',getTodo);
 route.delete('/todo/:firebaseUserId/:taskId',deleteTodo);
+route.get('/vendors',getVendors)
+route.get('/vendors/:id',getVendor)
+route.get('/vendors/:id/services',getServices)
+
 
 export default route;
