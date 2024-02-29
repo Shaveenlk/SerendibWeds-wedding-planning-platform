da0dfe954779878198a0e13ff56f55dae7c5f01b
jayathuc <jayathuchamikara@gmail.com>
2024-02-29 09:18:07 +0530

    Cleaned about us page

--- frontend/src/pages/AboutUsPage/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage/AboutUsPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AboutUsPage.css';
-// Import images for the team members if they are stored locally
+// Import images for the team members
 import jayathu from './jayathu.jpg'; 
 import joseph from './joseph.jpg'; 
 import janudi from './janudi.jpg'; 
@@ -14,7 +14,6 @@ import nishadiName from './nishadiName.png';
 
 
 function AboutUsPage() {
-    // ... your state or other variables
 
     return (
         <div className="about-us-container">
@@ -32,35 +31,7 @@ function AboutUsPage() {
                 <h1>Our Mission</h1>
                 <p>"Our mission is to make wedding planning as delightful as the day itself. We provide a comprehensive platform where users can effortlessly find exactly what they need, draw inspiration, and explore limitless possibilities with our advanced, NLP-powered search functionality."</p>
             </div>
-            
-            {/* <div className="section meet-our-team">
-                <h1>Meet Our Team</h1>
-                <div className="team-members">
-                <div className="team-member">
-                        <img src={jayathu} alt="Team Member Name" />
-                        <p>Team Member Name</p>
-                    </div>
-                    <div className="team-member">
-                        <img src={joseph} alt="Team Member Name" />
-                        <p>Team Member Name</p>
-                    </div>
-                    <div className="team-member">
-                        <img src={janudi} alt="Team Member Name" />
-                        <p>Team Member Name</p>
-                    </div>
-                    <div className="team-member">
-                        <img src={shaveen} alt="Team Member Name" />
-                        <p>Team Member Name</p>
-                    </div>
-                    <div className="team-member">
-                        <img src={nishadi} alt="Team Member Name" />
-                        <p>Team Member Name</p>
-                    </div>
-                </div>
-            </div> */}
 
-
-            {/* Choose a correct color - JC */}
             <div className="section meet-our-team"> 
             <h1>Meet Our Team</h1>
             <div className="team-members">
@@ -84,32 +55,9 @@ function AboutUsPage() {
                     <img src={nishadi} alt="Team Member Name" className="team-member-photo" />
                     <img src={nishadiName} alt="Team Member Name" className="team-member-name-photo" />
                 </div>
-                
-                
-                {/* <div className="team-member">
-                    <img src={jayathu} alt="Team Member Name" />
-                    <div className="team-member-name">Team Member Name</div>
-                </div>
-                <div className="team-member">
-                    <img src={joseph} alt="Team Member Name" />
-                    <div className="team-member-name">Team Member Name</div>
-                </div>
-                <div className="team-member">
-                    <img src={janudi} alt="Team Member Name" />
-                    <div className="team-member-name">Team Member Name</div>
-                </div>
-                <div className="team-member">
-                    <img src={shaveen} alt="Team Member Name" />
-                    <div className="team-member-name">Team Member Name</div>
-                </div>
-                <div className="team-member">
-                    <img src={nishadi} alt="Team Member Name" />
-                    <div className="team-member-name">Team Member Name</div>
-                </div> */}
             </div>
         </div>
         </div>
     );
 }
-
 export default AboutUsPage;
