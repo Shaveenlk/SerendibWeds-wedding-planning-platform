f8c5b75688f91a1a8b9132ee95dfd7bef6c0c434
Janudi225 <janudigr@gmail.com>
2024-03-16 10:00:28 +0530

    Recreated createVendorBooking function with appointment ID

--- backend/controller/bookingController.js
+++ backend/controller/bookingController.js
@@ -1,11 +1,23 @@
 import Vendors from "../model/vendorModel.js";
 import Users from "../model/userModel.js";
+import nodemailer from 'nodemailer';
+
+// Create a transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'serendibweds2@gmail.com', // Your Gmail address
+      pass: 'fvum olga mabt acyc', // Your Gmail password or app password
+    },
+  });
+
+
 
 // Create a new booking
 export const createVendorBooking = async (req, res) => {
     try {
         const { id } = req.params; 
-        const {appointments,firebaseUserId} =req.body;
+        const {appointments,firebaseUserId,appointmentId } =req.body;
 
         console.log(req.body)
 
@@ -18,12 +30,12 @@ export const createVendorBooking = async (req, res) => {
         }
         
         // Add the new appointment to the vendor's appointments array
-        vendor.appointments.push(...appointments);
+        vendor.appointments.push({...appointments,appointmentId });
 
         // Save the updated vendor document
         const updatedVendor = await vendor.save();
-         // Assuming there is a User model with name and email fields
-         const user = await Users.findOne({ firebaseUserId: firebaseUserId });
+        // Assuming there is a User model with name and email fields
+        const user = await Users.findOne({ firebaseUserId: firebaseUserId });
          if (!user) {
              return res.status(404).json({ message: "User not found" });
          }
@@ -34,7 +46,7 @@ export const createVendorBooking = async (req, res) => {
              bookingDate: appointments[0].bookingDate,
              bookingTime: appointments[0].bookingTime,
              email: vendor.email,
-             // Add other booking details as needed
+             appointmentId: appointmentId[0].appointmentId
          };
  
          // Add the booking to the user's bookings array
