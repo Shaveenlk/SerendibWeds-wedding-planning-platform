915f36d7c894c16980caff3a2318e89e0cc0da1a
jayathuc <jayathuchamikara@gmail.com>
2024-03-18 11:59:04 +0530

    Added routes for delete appointment

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ import { deleteTodo, getTodo, updateTodo,addTodo } from '../controller/todoContr
 import {getVendors,getVendor, checkVendor} from "../controller/vendorController.js"
 import { getServices } from '../controller/serviceController.js';
 import { getEventDetails } from '../controller/eventDetailsController.js';
+import { createVendorBooking, getAppointmentsByUser, deleteAppointment } from '../controller/bookingController.js';
+
 
 
 const route =express.Router();
@@ -20,5 +22,8 @@ route.get('/vendors/:id',getVendor)
 route.get('/vendors/:id/services',getServices)
 route.get('/checkvendor/:firebaseUserId', checkVendor)
 route.get('/vendorsEvents/:id',getEventDetails)
+route.post('/vendors/:id/booking',createVendorBooking)
+route.get('/users/:firebaseUserId/bookings', getAppointmentsByUser);
+route.delete('/bookings/:firebaseUserId/:appointmentId', deleteAppointment);
 
 export default route;
