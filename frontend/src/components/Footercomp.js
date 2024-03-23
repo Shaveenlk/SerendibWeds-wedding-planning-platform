b4bd54d17912d54892d47373455201ae9ff5249b
jayathuc <jayathuchamikara@gmail.com>
2024-03-23 16:30:44 +0530

    Updated links in footer, Created hereSecioncomp2, Added some optional images for hero section

--- frontend/src/components/Footercomp.js
+++ frontend/src/components/Footercomp.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Box, Container, Grid, Typography } from "@mui/material";
 import "./Footercomp.css"
+import { Link } from 'react-router-dom';
+
 
 // Define your custom color
 const customColor = "#DDBEA9"; // Replace this with your desired color code
@@ -27,28 +29,30 @@ const Footercomp = () => {
         <div className="link-boxes">
           <ul className="box">
             <li className="link_name">Company</li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Contact us</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/aboutus">About us</Link></li>
+            <li><Link to="/contact">Contact us</Link></li> {/* Assuming you'll add a route for this */}
           </ul>
           <ul className="box">
             <li className="link_name">Features</li>
-            <li><a href="#">DreamSearch</a></li>
-            <li><a href="#">My Profile</a></li>
-            <li><a href="#">Testomonials</a></li>
+            <li><Link to="/dreamsearch">DreamSearch</Link></li>
+            <li><Link to="/profile">My Profile</Link></li>
+            <li><Link to="/testimonials">Testimonials</Link></li> {/* Assuming you'll add a route for this */}
           </ul>
           <ul className="box">
             <li className="link_name">Contributions</li>
-            <li><a href="#">Testomonials</a></li>
-            <li><a href="#">Feedback</a></li>
+            <li><Link to="/testimonials">Testimonials</Link></li> {/* Assuming you'll add a route for this */}
+            <li><Link to="/feedback">Feedback</Link></li> {/* Assuming you'll add a route for this */}
           </ul>
           <ul className="box">
             <li className="link_name">Socials</li>
-            <li><a href="#">Instragram</a></li>
-            <li><a href="#">Facebook</a></li>
+            {/* External links can remain as <a> but ensure they have valid href attributes */}
+            <li><a href="https://www.instagram.com/serendibweds__/">Instagram</a></li>
+            <li><a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=61557243077413">Facebook</a></li>
           </ul>
           <ul className="box input-box">
             <li className="link_name">Subscribe to newsletter</li>
+            {/* Newsletter subscription form can stay as is, assuming it's handled via JS */}
             <li><input type="text" placeholder="Enter your email" /></li>
             <li><input type="button" value="Subscribe" /></li>
           </ul>
@@ -56,15 +60,17 @@ const Footercomp = () => {
       </div>
       <div className="bottom-details">
         <div className="bottom_text">
-          <span className="copyright_text">Copyright © 2023 <a href="#">SerendibWeds</a> All rights reserved</span>
+          {/* Keep external links or route to a dedicated privacy/terms component */}
+          <span className="copyright_text">Copyright © 2023 <Link to="/">SerendibWeds</Link> All rights reserved</span>
           <span className="policy_terms">
-            <a href="#">Privacy policy</a>
-            <a href="#">Terms & conditions</a>
+            <Link to="/privacy-policy">Privacy policy</Link> {/* Assuming you'll add a route for this */}
+            <Link to="/terms-conditions">Terms & conditions</Link> {/* Assuming you'll add a route for this */}
           </span>
         </div>
       </div>
     </footer>
   );
 };
+
 
 export default Footercomp;
